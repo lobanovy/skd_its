2c6ace3ba0506eaddd32ea5f5ab4251edf6d577e
Yuriy <yura.lobanov@gmail.com>
2017-09-20 12:13:39 +0300

    ver.2

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -14,12 +14,25 @@ class CardsController < ApplicationController
  end
   def create
     @card = Card.new(card_params)
-    @card.card_id = %x'curl 192.168.1.171'
-    if @card.save
-     redirect_to @card
+    @card.card_id = %x'curl 192.168.1.2'
+    # Перевірка чи є прив'язка групи до контролера, та чи вставлена картка у рідер
+    if ((CardGroup.where(:card_reader_group_id => @card.card_reader_group_id).exists?) and ( @card.card_id != 0))
+        # Перевірка чи існує вже картка у базі та її деактивація якщо вона активна
+        if Card.where(:enable => "1").find_by(card_id: @card.card_id)
+    	     @card_temp = Card.new
+    	     @card_temp = Card.where(:enable => "1").find_by(card_id: @card.card_id)
+    		 @card_temp.enable = "0"
+    		 @card_temp.save
+    	end	
+    	
+        if @card.save
+           redirect_to @card
+        else
+          render 'new'
+        end
     else
-     render 'new'
-    end
+    	redirect_to @card
+    end	
   end 
   
   private
