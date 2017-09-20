c954053326480c7b4f9c706827ae353ae7c58c81
Yuriy <yura.lobanov@gmail.com>
2017-09-21 00:23:14 +0300

    Finish events script

--- app/controllers/card_readers_controller.rb
+++ app/controllers/card_readers_controller.rb
@@ -8,11 +8,42 @@ class CardReadersController < ApplicationController
    end	
   def show
     @card_reader = CardReader.find(params[:id])
-    @events = (%x"curl -s #{@card_reader.address} |tr ':' ' '|tr '\n' ' '").split(' ')
-     respond_to do |format|
+    #Зчитування подій з контролера та запис їх у стрічковий масив
+    @raw_events = (%x"curl -s #{@card_reader.address} |tr ':' ' '|tr '\n' ' '").split(' ')
+    # Якщо контролер відповів:
+    if @raw_events.length > 0
+      # То обнуляється лічильник невдалих спроб
+      @card_reader.failure_count = 0
+      @card_reader.save 
+      i = 0
+      while i < @raw_events.length
+        event = Event.new
+        event.address = @card_reader.address
+        event.time = @raw_events[i].to_i
+        event.card_id = @raw_events[i+1].to_i
+        event.time_end = @raw_events[i+2].to_i
+        if event.time !=0
+          rec = Event.where(:address => event.address, :time => event.time, :card_id => event.card_id).exists?
+          if (!rec)
+            event.save
+          else
+            event = Event.find_by(address: event.address, time: event.time, card_id: event.card_id)
+            if (@raw_events[i+2].to_i) != (event.time_end) 
+              event.time_end = @raw_events[i+2].to_i
+              event.save
+            end  
+          end
+        end  
+        i = i + 3
+      end
+    else
+      @card_reader.failure_count = @card_reader.failure_count + 1
+      @card_reader.save 
+    end 
+    respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @card_reader}
-     end
+    end
   end
   def new
   	@card_reader = CardReader.new
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920123400) do
+ActiveRecord::Schema.define(version: 20170920185905) do
 
   create_table "card_groups", force: true do |t|
     t.integer "card_reader_id"
